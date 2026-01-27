4074a302417e81e197b9f43f428fb01e7230e108
jackdwyer <jackjack.dwyer@gmail.com>
2026-01-28 01:34:14 +1100

    support osx/alacritty

--- native-host/src/main.rs
+++ native-host/src/main.rs
@@ -1,9 +1,9 @@
 use byteorder::{NativeEndian, ReadBytesExt, WriteBytesExt};
 use indoc::formatdoc;
-use inotify::{Inotify, WatchMask};
 use log::{error, info, warn};
 use nix::sys::signal::{kill, Signal};
 use nix::unistd::Pid;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use scopeguard::defer;
 use serde::{Deserialize, Serialize};
 use simplelog::{LevelFilter, WriteLogger};
@@ -29,6 +29,16 @@ fn spawn_thread<T: Send + 'static>(done_sender: Sender<T>, f: impl 'static + Sen
 }
 
 fn spawn_editor(src_path: &Path) -> io::Result<(Child, Pid)> {
+    let is_macos = cfg!(target_os = "macos");
+
+    if is_macos {
+        spawn_editor_alacritty(src_path)
+    } else {
+        spawn_editor_gnome(src_path)
+    }
+}
+
+fn spawn_editor_gnome(src_path: &Path) -> io::Result<(Child, Pid)> {
     // Killing the gnome-terminal process launched here doesn't actually close the terminal,
     // because that is hosted by some daemon process. Instead we return the PID of the process
     // running inside of gnome-terminal. To learn its PID, we perform the following dance:
@@ -71,6 +81,110 @@ fn spawn_editor(src_path: &Path) -> io::Result<(Child, Pid)> {
         }
     }
     let pid = pid.trim().parse::<i32>().unwrap();
+    Ok((child, Pid::from_raw(pid)))
+}
+
+fn spawn_editor_alacritty(src_path: &Path) -> io::Result<(Child, Pid)> {
+    let src_path_disp = src_path.display();
+
+    let pid_dir = TempDir::new("editor-pid")?;
+
+    let tmp_pid_path = pid_dir.path().join("pid-tmp");
+    let tmp_pid_path_disp = tmp_pid_path.display();
+
+    let pid_path = pid_dir.path().join("pid");
+    let pid_path_disp = pid_path.display();
+
+    let child = Command::new("/Applications/Alacritty.app/Contents/MacOS/alacritty")
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .arg("-e")
+        .arg("sh")
+        .arg("-c")
+        .arg(formatdoc! {"
+            echo $$ > '{tmp_pid_path_disp}'
+            mv '{tmp_pid_path_disp}' '{pid_path_disp}'
+            exec vim '{src_path_disp}'
+        "})
+        .spawn()?;
+    let mut pid = String::new();
+    while pid.is_empty() {
+        if pid_path.exists() {
+            pid = fs::read_to_string(&pid_path)?;
+        } else {
+            thread::sleep(Duration::from_millis(5));
+        }
+    }
+    let pid = pid.trim().parse::<i32>().unwrap();
+    Ok((child, Pid::from_raw(pid)))
+}
+
+fn spawn_editor_iterm2(src_path: &Path) -> io::Result<(Child, Pid)> {
+    // On macOS, we use osascript to launch iTerm2 with a specific command.
+    // We need to track the PID of the editor process, similar to the Linux approach.
+    let src_path_disp = src_path.display();
+
+    let pid_dir = TempDir::new("editor-pid")?;
+
+    let tmp_pid_path = pid_dir.path().join("pid-tmp");
+    let tmp_pid_path_disp = tmp_pid_path.display();
+
+    let pid_path = pid_dir.path().join("pid");
+    let pid_path_disp = pid_path.display();
+
+    // Build the shell command that will write PID and exec vim
+    let shell_command = formatdoc! {"
+        echo $$ > '{tmp_pid_path_disp}'
+        mv '{tmp_pid_path_disp}' '{pid_path_disp}'
+        exec vim '{src_path_disp}'
+    "};
+
+    // Build the AppleScript to run in iTerm2
+    // Note: We can't use --wait with iTerm2, so we spawn a dummy child process
+    // that will be killed when we detect the editor has exited.
+    let apple_script = format!(
+        r#"tell application "iTerm2"
+            create window with default profile command "{}"
+        end tell"#,
+        shell_command.replace('"', r#"\""#)
+    );
+
+    // Spawn osascript in background
+    let child = Command::new("osascript")
+        .arg("-e")
+        .arg(&apple_script)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()?;
+
+    // Wait for the PID file to appear
+    let mut pid = String::new();
+    let mut attempts = 0;
+    while pid.is_empty() && attempts < 100 {
+        if pid_path.exists() {
+            pid = fs::read_to_string(&pid_path)?;
+        } else {
+            thread::sleep(Duration::from_millis(50));
+            attempts += 1;
+        }
+    }
+
+    if pid.is_empty() {
+        return Err(io::Error::new(
+            io::ErrorKind::TimedOut,
+            "Timeout waiting for iTerm2 to write PID file",
+        ));
+    }
+
+    let pid = pid.trim().parse::<i32>().map_err(|e| {
+        io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!("Failed to parse PID: {}", e),
+        )
+    })?;
+
     Ok((child, Pid::from_raw(pid)))
 }
 
@@ -240,15 +354,19 @@ fn handle_messages(tmp_dir: &Path, exit: Sender<io::Result<()>>) -> io::Result<(
 
 fn send_updates(src_path: &Path, content_type: ContentType) -> io::Result<()> {
     let mut last_html: Option<String> = None;
-    let mut inotify = Inotify::init()?;
-    let mask = WatchMask::CLOSE_WRITE
-        | WatchMask::CREATE
-        | WatchMask::DELETE
-        | WatchMask::MODIFY
-        | WatchMask::MOVE_SELF
-        | WatchMask::MOVED_TO;
-    inotify.add_watch(src_path, mask)?;
-    let mut buffer = [0; 1024];
+    let (tx, rx) = channel();
+    let mut watcher: RecommendedWatcher = Watcher::new(
+        move |res: Result<notify::Event, _>| {
+            let _ = tx.send(res);
+        },
+        notify::Config::default(),
+    )
+    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+    watcher
+        .watch(src_path, RecursiveMode::NonRecursive)
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
     loop {
         info!("Checking for updates in source");
         let html = match content_type {
@@ -261,23 +379,9 @@ fn send_updates(src_path: &Path, content_type: ContentType) -> io::Result<()> {
             let message = HostMessage::ReplaceAll { content: html };
             write_message(&message, &mut stdout().lock())?;
         }
-        inotify.read_events_blocking(&mut buffer)?;
-        loop {
-            match inotify.read_events(&mut buffer) {
-                Ok(mut events) => {
-                    if events.next().is_none() {
-                        break;
-                    }
-                }
-                Err(err) => {
-                    if err.kind() == io::ErrorKind::WouldBlock {
-                        break;
-                    } else {
-                        return Err(err);
-                    }
-                }
-            }
-        }
+        let _ = rx
+            .recv()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
     }
 }
 
@@ -311,4 +415,181 @@ fn main() -> io::Result<()> {
     disconnect(&mut stdout().lock())?;
     info!("Exiting");
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::{self, File};
+    use std::io::Write;
+    use tempfile::TempDir;
+
+    #[test]
+    fn test_spawn_editor_gnome_command_structure() {
+        let temp_dir = TempDir::new().unwrap();
+        let src_path = temp_dir.path().join("test-file.txt");
+        let src_path_disp = src_path.display();
+        let pid_dir = temp_dir.path().join("pid-dir");
+        fs::create_dir(&pid_dir).unwrap();
+        let tmp_pid_path = pid_dir.join("pid-tmp");
+        let tmp_pid_path_disp = tmp_pid_path.display();
+        let pid_path = pid_dir.join("pid");
+        let pid_path_disp = pid_path.display();
+
+        let shell_command = formatdoc! {"
+            echo $$ > '{tmp_pid_path_disp}'
+            mv '{tmp_pid_path_disp}' '{pid_path_disp}'
+            exec vim '{src_path_disp}'
+        "};
+
+        assert!(shell_command.contains(&format!("exec vim '{}'", src_path_disp)));
+        assert!(shell_command.contains(&format!("echo $$ > '{}'", tmp_pid_path_disp)));
+        assert!(shell_command.contains(&format!("mv '{}' '{}'", tmp_pid_path_disp, pid_path_disp)));
+    }
+
+    #[test]
+    fn test_spawn_editor_iterm2_applescript_structure() {
+        let temp_dir = TempDir::new().unwrap();
+        let src_path = temp_dir.path().join("test-file.txt");
+        let src_path_disp = src_path.display();
+        let tmp_pid_path = temp_dir.path().join("pid-tmp");
+        let tmp_pid_path_disp = tmp_pid_path.display();
+        let pid_path = temp_dir.path().join("pid");
+        let pid_path_disp = pid_path.display();
+
+        let shell_command = formatdoc! {"
+            echo $$ > '{tmp_pid_path_disp}'
+            mv '{tmp_pid_path_disp}' '{pid_path_disp}'
+            exec vim '{src_path_disp}'
+        "};
+
+        let apple_script = format!(
+            r#"tell application "iTerm2"
+            create window with default profile command "{}"
+        end tell"#,
+            shell_command.replace('"', r#"\""#)
+        );
+
+        assert!(apple_script.contains("tell application \"iTerm2\""));
+        assert!(apple_script.contains("create window with default profile command"));
+        assert!(apple_script.contains(&format!("exec vim '{}'", src_path_disp)));
+    }
+
+    #[test]
+    fn test_send_updates_watches_file() {
+        let temp_dir = TempDir::new().unwrap();
+        let src_path = temp_dir.path().join("test-file.txt");
+        File::create(&src_path).unwrap();
+
+        let (tx, _rx) = channel();
+        let watcher_result: io::Result<RecommendedWatcher> = Watcher::new(
+            move |res: Result<notify::Event, _>| {
+                let _ = tx.send(res);
+            },
+            notify::Config::default(),
+        )
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+
+        assert!(watcher_result.is_ok(), "Watcher creation failed");
+
+        let mut watcher = watcher_result.unwrap();
+        let watch_result = watcher
+            .watch(&src_path, RecursiveMode::NonRecursive)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+
+        assert!(watch_result.is_ok(), "Failed to watch file");
+    }
+
+    #[test]
+    fn test_spawn_editor_platform_selection() {
+        let is_macos = cfg!(target_os = "macos");
+        let is_linux = cfg!(target_os = "linux");
+
+        assert!(
+            is_macos || is_linux,
+            "spawn_editor should be tested on macOS or Linux"
+        );
+    }
+
+    #[test]
+    fn test_spawn_editor_iterm2_escaped_quotes() {
+        let temp_dir = TempDir::new().unwrap();
+        let src_path = temp_dir.path().join("test-\"quote\".txt");
+        let src_path_disp = src_path.display();
+        let tmp_pid_path = temp_dir.path().join("pid-tmp");
+        let tmp_pid_path_disp = tmp_pid_path.display();
+        let pid_path = temp_dir.path().join("pid");
+        let pid_path_disp = pid_path.display();
+
+        let shell_command = formatdoc! {"
+            echo $$ > '{tmp_pid_path_disp}'
+            mv '{tmp_pid_path_disp}' '{pid_path_disp}'
+            exec vim '{src_path_disp}'
+        "};
+
+        let apple_script = format!(
+            r#"tell application "iTerm2"
+            create window with default profile command "{}"
+        end tell"#,
+            shell_command.replace('"', r#"\""#)
+        );
+
+        assert!(!apple_script.contains(r#"test-"quote".txt"#));
+        assert!(apple_script.contains(r#"test-\"quote\".txt"#));
+    }
+
+    #[test]
+    fn test_spawn_editor_gnome_command_arguments() {
+        let temp_dir = TempDir::new().unwrap();
+        let src_path = temp_dir.path().join("test.txt");
+
+        let pid_dir = TempDir::new().unwrap();
+        let tmp_pid_path = pid_dir.path().join("pid-tmp");
+        let tmp_pid_path_disp = tmp_pid_path.display();
+        let pid_path = pid_dir.path().join("pid");
+        let pid_path_disp = pid_path.display();
+        let src_path_disp = src_path.display();
+
+        let shell_command = formatdoc! {"
+            echo $$ > '{tmp_pid_path_disp}'
+            mv '{tmp_pid_path_disp}' '{pid_path_disp}'
+            exec vim '{src_path_disp}'
+        "};
+
+        assert!(shell_command.contains("exec"));
+        assert!(shell_command.contains("vim"));
+        assert!(shell_command.contains(&src_path_disp.to_string()));
+    }
+
+    #[test]
+    fn test_file_content_reading() {
+        let temp_dir = TempDir::new().unwrap();
+        let src_path = temp_dir.path().join("test.txt");
+        let content = "test content";
+
+        let mut file = File::create(&src_path).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+
+        let read_content = fs::read_to_string(&src_path).unwrap();
+        assert_eq!(read_content, content);
+    }
+
+    #[test]
+    fn test_pid_file_creation() {
+        let temp_dir = TempDir::new().unwrap();
+        let tmp_pid_path = temp_dir.path().join("pid-tmp");
+        let pid_path = temp_dir.path().join("pid");
+
+        let pid = "12345";
+
+        let mut file = File::create(&tmp_pid_path).unwrap();
+        file.write_all(pid.as_bytes()).unwrap();
+        fs::rename(&tmp_pid_path, &pid_path).unwrap();
+
+        assert!(pid_path.exists());
+        assert!(!tmp_pid_path.exists());
+
+        let read_pid = fs::read_to_string(&pid_path).unwrap();
+        assert_eq!(read_pid.trim(), pid);
+    }
 }
